bdd2b33afafe02cc3f37a697b8150a53d8775fa3
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2021-10-19 23:52:01 +0200

    Even distribution of points

--- Assets/Resources/PointPlotter.cs
+++ Assets/Resources/PointPlotter.cs
@@ -9,10 +9,13 @@ public class PointPlotter : MonoBehaviour
 
     public float turnFraction = 0.000000f;
 
+    public float pow = 1f;
+
     public GameObject pointPrefab;
     private Transform[] points;
 
     public bool incrementFraction = true;
+    public bool incrementPow = true;
 
 
     private void Awake() {
@@ -29,16 +32,21 @@ public class PointPlotter : MonoBehaviour
         if (incrementFraction) {
             IncrementTurnFraction();
         }
+        if (incrementPow) {
+            IncrementPow();
+        }
     }
 
     private void PlotPoints() {
         Vector3 position = new Vector3(0, 0, 0);
         for (int i = 0; i < numberOfPoints; ++i) {
-            float distance = i / (numberOfPoints - 0.001f);
+            float distance = Mathf.Pow(i / (numberOfPoints + 1f), pow);
             float angle = 2 * Mathf.PI * turnFraction * i;
 
             float x = 5 * distance * Mathf.Cos(angle);
             float y = 5 * distance * Mathf.Sin(angle);
+
+            // Debug.Log(string.Format("distance = {0}, angle = {0}, x = {0}, y = {0}", distance, angle, x, y));
 
             position.Set(x, y, 0);
             points[i].position = position;
@@ -46,6 +54,10 @@ public class PointPlotter : MonoBehaviour
     }
 
     private void IncrementTurnFraction() {
-        turnFraction += 0.000001f;
+        turnFraction += 0.000005f;
+    }
+
+    private void IncrementPow() {
+        pow += 0.01f;
     }
 }
